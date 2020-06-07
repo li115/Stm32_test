4972a91b3e0bef73d06ed0f270e5993858eadf46
li <gzl_zk@163.com>
2020-06-07 17:38:29 +0800

    添加串口驱动

--- Project/USER/main.c
+++ Project/USER/main.c
@@ -11,31 +11,42 @@
 
  int main(void)
  {
+    u8 t;
+    u8 len;	
+    u16 times=0; 
     delay_init();       //延时函数初始化	  
     LED_Init();         //初始化与LED连接的硬件接口
     KEY_Init();         //按键初始化
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断分级优先组2
+     uart_init(9600);        //串口初始化
     
     while(1)
     {
-        u8 keypres = KEY_Scan(ENABLE_MORE_PRESS);
-        if(keypres == NOKEY_PRES)
-            continue;
-        else if(keypres == KEY0_PRES)
+        if(USART_RX_STA&0x8000)
         {
-            LED0=0;
-            LED1=0;
-        }
-        else if(keypres == KEY1_PRES)
+            len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
+            printf("\r\n您发送的消息为:\r\n");
+            for(t=0;t<len;t++)
+            {
+                USART1->DR=USART_RX_BUF[t];
+                while((USART1->SR&0X40)==0);//等待发送结束
+            }
+            printf("\r\n\r\n");//插入换行
+            USART_RX_STA=0;
+        }else
         {
-            LED0=1;
-            LED1=0;
+            times++;
+            if(times%5000==0)
+            {
+                printf("\r\nALIENTEK MiniSTM32开发板 串口实验\r\n");
+                printf("正点原子@ALIENTEK\r\n\r\n\r\n");
+            }
+            if(times%200==0)printf("请输入数据,以回车键结束\r\n");  
+            if(times%30==0)LED0=!LED0;//闪烁LED,提示系统正在运行.
+            delay_ms(10);   
         }
-        else
-        {
-            LED0=1;
-            LED1=1;
-        }
-    }
+    }	 
  }
 
 
